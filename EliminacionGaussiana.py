06849419adcd25697dddccd238583137a0440b3b
IvanPedroSuarez <23030344@itesa.edu.mx>
2025-03-10 08:00:39 -0600

    2do Intento de hacer el llenado de matrices

--- EliminacionGaussiana.py
+++ EliminacionGaussiana.py
@@ -1,33 +1,21 @@
-matriz = {}
+matriz = []
+
 def llenarmatriz():
-    matriz = {}
-    print('Metodo para llenar una matriz 3 * 3')
-    for op in range(9):
-
-        valor = int(input('Ingrese el valor del valor ' + op ))
-        matriz = {op}
-
+    global matriz
+    matriz = []
+    print('Método para llenar una matriz 3x3')
+    for i in range(3):
+        fila = []
+        for j in range(3):
+            valor = int(input(f'Ingrese el valor para la posición [{i}][{j}]: '))
+            fila.append(valor)
+        matriz.append(fila)
 
 def mostrarmatriz():
-    for kl in range(matriz):
-        print(kl)
-
-
-
-
-
-
-
-
+    for fila in matriz:
+        for elemento in fila:
+            print(elemento, end=' ')
+        print()
 
 llenarmatriz()
 mostrarmatriz()
-
-
-
-
-
-
-
-
-
