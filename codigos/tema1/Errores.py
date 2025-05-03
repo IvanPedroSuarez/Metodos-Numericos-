1488fa988b30572d9cb88d50286d6bf03d8c3c59
IvanPedroSuarez <23030344@itesa.edu.mx>
2025-05-03 13:29:29 -0600

    Update Errores.py

--- codigos/tema1/Errores.py
+++ codigos/tema1/Errores.py
@@ -9,6 +9,8 @@ print("Suma esperada:", 10**7 * 0.000001)
 
 print(suma)
 
+
+#Resultado al ejecutar en consola 
 Ejemplo 1 de error de precisión en Python  
 Suma obtenida: 1.0000000000007918  
 Suma esperada: 1.0
